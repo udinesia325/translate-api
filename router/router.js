10785e3680067b71d1efc76d9bb9af95b6df4894
udinesia325 <udinesia325@gmail.com>
2021-06-23 16:41:56 +0700

    menambahkan deskripsi di awal route

--- router/router.js
+++ router/router.js
@@ -1,6 +1,15 @@
 const express = require("express")
 const router = express.Router()
 const translateText = require("../controller/controller")
+router.get("/", (req, res) => {
+	const data = `https://fahruddinsalim.herokuapp.com/terjemah?teks=coba&kode=en 
+<br>
+atau pelajari lebih lanjut di dokumentasi github
+<a href="https://github.com/udinesia325/translate-api" target="_blank">klik disini</a>
+
+`
+	res.send(data)
+})
 router.get("/terjemah", translateText)
 
 
